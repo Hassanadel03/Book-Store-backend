2f33443cb23ab2ae3b94ce15cfd68daf5dfc9c7b
Hassanadel03 <hassanadel20003@gmail.com>
2023-12-06 16:17:55 +0200

    make Schema - BookAPI

---
+++ models/book.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bookSchema = new Schema(
+    {
+        bookName: { type: String, unique: true, required: true },
+        pdfName: { type: String, unique: true, required: true },
+        info: { type: String, required: true },
+    },
+    {
+        timestamps: true
+    }
+
+);
+module.exports = mongoose.model('Book', bookSchema);
---
+++ models/user.model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+const userSchema = new Schema(
+    {
+        fullName: { type: String, required: true },
+        email: { type: String, unique: true, required: true },
+        password: { type: String, required: true },
+        phoneNumber: { type: String, required: false },
+        image: { type: String, required: false },
+        skills: { type: Array, required: false }
+    },
+    {
+        timestamps: true
+    }
+
+);
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+
+    try {
+        const salt = await bcrypt.genSalt(10); // Generate a salt
+        const hashedPassword = await bcrypt.hash(this.password, salt); // Hash the password
+        this.password = hashedPassword; // Store the hashed password
+        next();
+    } catch (error) {
+        return next(error);
+    }
+});
+module.exports = mongoose.model('User', userSchema);
